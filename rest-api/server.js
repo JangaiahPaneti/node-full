5ae0bbf65268083c8e8bded7670affcb4faa55cd
Jangaiah Paneti <jangaiah_paneti@epam.com>
2024-01-17 20:48:13 +0530

    Server added

--- rest-api/server.js
+++ rest-api/server.js
@@ -1,1 +1,26 @@
-// To be added
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/api/user', (req, res) => {
+    const data = {
+      userId: 1,
+      firstName: 'Rama',
+      middleName: "Chandra",
+      lastName: "Surya"
+    };
+    res.json(data);
+  });
+
+  
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
